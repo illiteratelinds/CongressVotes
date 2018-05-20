6e5aa2e3c1dc5144f67bef56c9713c63b8c94fa8
illiteratelinds <lindsay.padian@gmail.com>
2018-05-20 17:44:12 -0400

    rake task to populate database

--- app/models/legislator.rb
+++ app/models/legislator.rb
@@ -1,3 +1,4 @@
 class Legislator < ApplicationRecord
   belongs_to :state
+  # validates :first_name, :middle_name, :last_name, :party, :title, :chamber, :term_start, :term_end, :missed_votes_pct, presence: true
 end
--- app/models/propublica_api_caller.rb
+++ app/models/propublica_api_caller.rb
@@ -1,21 +1,39 @@
+require 'pry-byebug'
+
 class PropublicaApiCaller
 
+  @attributes = ["state", "first_name", "middle_name", "last_name", "party", "title", "chamber", "term_start", "term_end", "missed_votes_pct"]
 
-  # house_legislator_data = HTTParty.get('https://api.propublica.org/congress/v1/house/votes/recent.json', :headers => { 'Content-Type' => 'application/json', 'X-API-Key' => PROPUBLICA_API_KEY })
-  # puts house_legislator_data
+  def self.update_legislators
+    Legislator.destroy_all
+    senate_member_data = HTTParty.get("https://api.propublica.org/congress/v1/115/senate/members.json", :headers => { 'Content-Type' => 'application/json', 'X-API-Key' => ENV["PROPUBLICA_API_KEY"] })
 
-  senate_legislator_data = HTTParty.get('https://api.propublica.org/congress/v1/senate/votes/recent.json', :headers => { 'Content-Type' => 'application/json', 'X-API-Key' => PROPUBLICA_API_KEY })
-  puts senate_legislator_data
+    senate_member_data["results"][0]["members"].each do |legislator_json|
+      legislator = Legislator.new
+      @attributes.each do |attribute|
+        if attribute != "state"
+          legislator.send("#{attribute}=", legislator_json[attribute])
+        else
+          state = State.find_by(abbreviation: legislator_json["state"])
+          legislator.state_id = state.id
+        end
+      end
+      legislator.save!
+    end
+    house_member_data = HTTParty.get("https://api.propublica.org/congress/v1/115/house/members.json", :headers => { 'Content-Type' => 'application/json', 'X-API-Key' => ENV["PROPUBLICA_API_KEY"] })
 
-  # house_vote_data = HTTParty.get('https://api.propublica.org/congress/v1/house/votes/recent.json', :headers => { 'Content-Type' => 'application/json', 'X-API-Key' => PROPUBLICA_API_KEY })
-  # puts house_vote_data
-  
-  # senate_vote_data = HTTParty.get('https://api.propublica.org/congress/v1/senate/votes/recent.json', :headers => { 'Content-Type' => 'application/json', 'X-API-Key' => PROPUBLICA_API_KEY })
-  # puts senate_vote_data
-
-
-  def update_legislators
-    # start with current legislators
-    # add new legislators after elections
+    house_member_data["results"][0]["members"].each do |legislator_json|
+      legislator = Legislator.new
+      @attributes.each do |attribute|
+        if attribute != "state"
+          legislator.send("#{attribute}=", legislator_json[attribute])
+        else
+          state = State.find_by(abbreviation: legislator_json["state"])
+          # binding.pry
+          legislator.state_id = state.id
+        end
+      end
+      legislator.save!
+    end
   end
 end
