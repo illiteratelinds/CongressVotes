555250d34c4e4ad339057fb76c41cc431530f412
illiteratelinds <lindsay.padian@gmail.com>
2018-05-20 17:52:12 -0400

    refactored rake task

--- app/models/legislator.rb
+++ app/models/legislator.rb
@@ -1,4 +1,3 @@
 class Legislator < ApplicationRecord
   belongs_to :state
-  # validates :first_name, :middle_name, :last_name, :party, :title, :chamber, :term_start, :term_end, :missed_votes_pct, presence: true
 end
--- app/models/propublica_api_caller.rb
+++ app/models/propublica_api_caller.rb
@@ -3,37 +3,27 @@ require 'pry-byebug'
 class PropublicaApiCaller
 
   @attributes = ["state", "first_name", "middle_name", "last_name", "party", "title", "chamber", "term_start", "term_end", "missed_votes_pct"]
+  @chambers = ["senate", "house"]
 
   def self.update_legislators
     Legislator.destroy_all
-    senate_member_data = HTTParty.get("https://api.propublica.org/congress/v1/115/senate/members.json", :headers => { 'Content-Type' => 'application/json', 'X-API-Key' => ENV["PROPUBLICA_API_KEY"] })
 
-    senate_member_data["results"][0]["members"].each do |legislator_json|
-      legislator = Legislator.new
-      @attributes.each do |attribute|
-        if attribute != "state"
-          legislator.send("#{attribute}=", legislator_json[attribute])
-        else
-          state = State.find_by(abbreviation: legislator_json["state"])
-          legislator.state_id = state.id
-        end
-      end
-      legislator.save!
-    end
-    house_member_data = HTTParty.get("https://api.propublica.org/congress/v1/115/house/members.json", :headers => { 'Content-Type' => 'application/json', 'X-API-Key' => ENV["PROPUBLICA_API_KEY"] })
+    @chambers.each do |chamber|
 
-    house_member_data["results"][0]["members"].each do |legislator_json|
-      legislator = Legislator.new
-      @attributes.each do |attribute|
-        if attribute != "state"
-          legislator.send("#{attribute}=", legislator_json[attribute])
-        else
-          state = State.find_by(abbreviation: legislator_json["state"])
-          # binding.pry
-          legislator.state_id = state.id
+      member_data = HTTParty.get("https://api.propublica.org/congress/v1/115/#{chamber}/members.json", :headers => { 'Content-Type' => 'application/json', 'X-API-Key' => ENV["PROPUBLICA_API_KEY"] })
+
+      member_data["results"][0]["members"].each do |legislator_json|
+        legislator = Legislator.new
+        @attributes.each do |attribute|
+          if attribute != "state"
+            legislator.send("#{attribute}=", legislator_json[attribute])
+          else
+            state = State.find_by(abbreviation: legislator_json["state"])
+            legislator.state_id = state.id
+          end
         end
+        legislator.save!
       end
-      legislator.save!
     end
   end
 end
